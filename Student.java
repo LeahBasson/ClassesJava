60460b0c81ead2037c5eee89ee414d17141b88f6
LeahBasson <leahbasson54@gmail.com>
2025-08-22 08:36:06 +0200

    :tada:

---
+++ Student.java
@@ -0,0 +1,60 @@
+package usesstudent;
+
+public class Student {
+
+    // attributes
+    private long studNr;
+    private String studName;
+    private double studBalance;
+
+    // setter and getters methods
+    public void setStudNr(long studNr) {
+        this.studNr = studNr;
+    }
+
+    public void setStudName(String studName) {
+        this.studName = studName;
+    }
+
+    public void setStudBalance(double studBalance) {
+        this.studBalance = studBalance;
+    }
+
+    public long getStudNr() {
+        return this.studNr;
+    }
+
+    public String getStudName() {
+        return this.studName;
+    }
+
+    public double getStudBalance() {
+        return this.studBalance;
+    }
+
+    // constructor
+    public Student() {
+
+    }
+
+    public Student(long studNr, String studName, double studBalance) {
+        this.studNr = studNr;
+        this.studName = studName;
+        this.studBalance = studBalance;
+    }
+
+    public void show() {
+//        getStudNr();
+//        getStudName();
+//        getStudBalance();
+        System.out.println("Student number: " + studNr);
+        System.out.println("Student name: " + studName);
+        System.out.println("Outstanding balance: " + studBalance);
+    }
+
+    public void makePayment(double studPayment) {
+        double total = studBalance - studPayment;
+        System.out.println("New ampount: " + total);
+    }
+
+} // end of class
---
+++ UsesStudent.java
@@ -0,0 +1,39 @@
+package usesstudent;
+
+import java.util.Scanner;
+
+public class UsesStudent {
+
+    public static void main(String[] args) {
+        // must instantiate a Student object (called stud1)
+
+        Scanner kbd = new Scanner(System.in);
+
+        // declaring a new variable so the name needs to be different.
+        long cStudNr;
+        String cStudName;
+        double cStudBalance;
+        double studPayment;
+
+        System.out.print("Enter student number: ");
+        cStudNr = kbd.nextLong();
+
+        System.out.print("Enter student name: ");
+        cStudName = kbd.next();
+
+        System.out.print("Enter outstanding balance: ");
+        cStudBalance = kbd.nextDouble();
+
+        Student stud1 = new Student(cStudNr, cStudName, cStudBalance);
+
+        // to access the show() method.
+        stud1.show();
+
+        System.out.print("Please enter payment amount: ");
+        studPayment = kbd.nextDouble();
+
+        stud1.makePayment(studPayment);
+
+    } // end of main
+
+} // end of class
